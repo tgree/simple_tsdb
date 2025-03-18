01b2fe6a1ac0a30e43f15120591dc87399b63fa0
Terry Greeniaus <terrygreeniaus@gmail.com>
2025-03-18 00:43:07 -0600

    bit less garbage

--- grafana/tgree-simpletsdb-datasource/src/components/QueryEditor/QueryEditor.tsx
+++ grafana/tgree-simpletsdb-datasource/src/components/QueryEditor/QueryEditor.tsx
@@ -1,15 +1,13 @@
 import React, { ReactElement } from 'react';
-import { InlineFieldRow, InlineField, Select } from '@grafana/ui';
+import { InlineField, Select } from '@grafana/ui';
 import { useMeasurements } from './useMeasurements';
-import { useSelectableValue } from './useSelectableValue';
 import { useChangeSelectableValue } from './useChangeSelectableValue';
 import type { EditorProps } from './types';
 
 export function QueryEditor(props: EditorProps): ReactElement {
-  const { datasource, query } = props;
+  const { datasource } = props;
 
-  const { loading, measurements, error } = useMeasurements(datasource);
-  const queryType = useSelectableValue(query.queryType);
+  const asyncMeasurementsState = useMeasurements(datasource);
 
   const onChangeMeasurement = useChangeSelectableValue(props, {
     propertyName: 'measurement',
@@ -18,18 +16,15 @@ export function QueryEditor(props: EditorProps): ReactElement {
 
   return (
     <>
-      <InlineFieldRow>
-        <InlineField label="Measurement" grow>
-          <Select
-            inputId="editor-databases"
-            options={measurements}
-            onChange={onChangeMeasurement}
-            isLoading={loading}
-            disabled={!!error}
-            value={queryType}
-          />
-        </InlineField>
-      </InlineFieldRow>
+      <InlineField label="Measurement" grow>
+        <Select
+          inputId="editor-databases"
+          options={asyncMeasurementsState.measurements}
+          onChange={onChangeMeasurement}
+          isLoading={asyncMeasurementsState.loading}
+          disabled={!!asyncMeasurementsState.error}
+        />
+      </InlineField>
     </>
   );
 }
