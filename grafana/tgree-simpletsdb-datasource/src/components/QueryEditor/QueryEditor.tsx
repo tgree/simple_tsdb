04a3483714c6f1ff1166d5d527fee3dbdde2c6a9
Terry Greeniaus <terrygreeniaus@gmail.com>
2025-03-18 01:41:47 -0600

    messing around

--- grafana/tgree-simpletsdb-datasource/src/components/QueryEditor/QueryEditor.tsx
+++ grafana/tgree-simpletsdb-datasource/src/components/QueryEditor/QueryEditor.tsx
@@ -23,15 +23,41 @@ export function QueryEditor(props: EditorProps): ReactElement {
     [props]
   );
 
+  const onChangeSeries = useCallback(
+    (selectable: SelectableValue<string>) => {
+    },
+    []
+  );
+
+  const onChangeField = useCallback(
+    (selectable: SelectableValue<string>) => {
+    },
+    []
+  );
+
   return (
     <>
-      <InlineField label="Measurement" grow>
+      <InlineField label="Measurement" labelWidth={16}>
         <Select
           inputId="editor-databases"
           options={asyncMeasurementsState.measurements}
           onChange={onChangeMeasurement}
           isLoading={asyncMeasurementsState.loading}
           disabled={!!asyncMeasurementsState.error}
+        />
+      </InlineField>
+      <InlineField label="Series" labelWidth={16}>
+        <Select
+          inputId="editor-series"
+          onChange={onChangeSeries}
+          disabled={true}
+        />
+      </InlineField>
+      <InlineField label="Field" labelWidth={16}>
+        <Select
+          inputId="editor-field"
+          onChange={onChangeField}
+          disabled={true}
         />
       </InlineField>
     </>
