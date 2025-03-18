f24c5e03ed5764cbdb5bee1b1a1bd6678e36ec44
Terry Greeniaus <terrygreeniaus@gmail.com>
2025-03-18 00:34:33 -0600

    garbage but have a measurement popup now

--- grafana/tgree-simpletsdb-datasource/src/components/ConfigEditor/ConfigEditor.tsx
+++ grafana/tgree-simpletsdb-datasource/src/components/ConfigEditor/ConfigEditor.tsx
@@ -1,46 +1,21 @@
 import React, { ReactElement } from 'react';
-import { InlineField, Input, SecretInput } from '@grafana/ui';
+import { InlineField, Input } from '@grafana/ui';
 import type { EditorProps } from './types';
 import { useChangeOptions } from './useChangeOptions';
-import { useChangeSecureOptions } from './useChangeSecureOptions';
-import { useResetSecureOptions } from './useResetSecureOptions';
 
 export function ConfigEditor(props: EditorProps): ReactElement {
-  const { jsonData, secureJsonData, secureJsonFields } = props.options;
-  const onTimeFieldChange = useChangeOptions(props, 'defaultTimeField');
-  const onPathFieldChange = useChangeOptions(props, 'path');
-  const onApiKeyChange = useChangeSecureOptions(props, 'apiKey');
-  const onResetApiKey = useResetSecureOptions(props, 'apiKey');
+  const { jsonData } = props.options;
+  const onDatabaseFieldChange = useChangeOptions(props, 'database');
 
   return (
     <>
-      <InlineField label="Path" labelWidth={14} interactive tooltip={'Json field returned to frontend'}>
+      <InlineField label="Database" labelWidth={14} interactive tooltip={'Database'}>
         <Input
-          id="config-editor-path"
-          onChange={onPathFieldChange}
-          value={jsonData.path}
-          placeholder="Enter the path, e.g. /api/v1"
+          id="config-editor-database"
+          onChange={onDatabaseFieldChange}
+          value={jsonData.database}
+          placeholder="Enter the database"
           width={40}
-        />
-      </InlineField>
-      <InlineField
-        label="Time Field"
-        labelWidth={14}
-        interactive
-        tooltip="Default time field used when interpolate the $__timeFilter()"
-      >
-        <Input onChange={onTimeFieldChange} placeholder="time" value={jsonData?.defaultTimeField ?? ''} width={40} />
-      </InlineField>
-      <InlineField label="API Key" labelWidth={14} interactive tooltip={'Secure json field (backend only)'}>
-        <SecretInput
-          required
-          id="config-editor-api-key"
-          isConfigured={secureJsonFields.apiKey}
-          value={secureJsonData?.apiKey}
-          placeholder="Enter your API key"
-          width={40}
-          onReset={onResetApiKey}
-          onChange={onApiKeyChange}
         />
       </InlineField>
     </>
--- grafana/tgree-simpletsdb-datasource/src/datasource.ts
+++ grafana/tgree-simpletsdb-datasource/src/datasource.ts
@@ -1,24 +1,34 @@
 import { DataSourceWithBackend, getTemplateSrv } from '@grafana/runtime';
 import type { DataSourceInstanceSettings, ScopedVars } from '@grafana/data';
-import type { BasicQuery, BasicDataSourceOptions, QueryTypesResponse } from './types';
+import type { BasicQuery, BasicDataSourceOptions, DatabasesResponse, MeasurementsResponse } from './types';
 
 export class BasicDataSource extends DataSourceWithBackend<BasicQuery, BasicDataSourceOptions> {
+  database: string;
+
   constructor(instanceSettings: DataSourceInstanceSettings<BasicDataSourceOptions>) {
     super(instanceSettings);
+
+    this.database = instanceSettings.jsonData.database || "";
   }
 
   applyTemplateVariables(query: BasicQuery, scopedVars: ScopedVars) {
     return {
       ...query,
-      rawQuery: getTemplateSrv().replace(query.rawQuery, scopedVars),
+      measurement: getTemplateSrv().replace(query.measurement, scopedVars),
+      series: getTemplateSrv().replace(query.series, scopedVars),
+      field: getTemplateSrv().replace(query.field, scopedVars),
     };
   }
 
   filterQuery(query: BasicQuery): boolean {
-    return !!query.rawQuery;
+    return true;
   }
 
-  getAvailableQueryTypes(): Promise<QueryTypesResponse> {
-    return this.getResource('/query-types');
+  getDatabases(): Promise<DatabasesResponse> {
+    return this.getResource('/databases');
+  }
+
+  getMeasurements(database: string): Promise<MeasurementsResponse> {
+    return this.getResource('/measurements?database=' + database);
   }
 }
