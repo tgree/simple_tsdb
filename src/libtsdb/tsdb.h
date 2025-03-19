09510dbaac3ca0691b95284c7fd8cf1a07b4c8c5
Terry Greeniaus <terrygreeniaus@gmail.com>
2025-03-19 01:26:56 -0600

    libtsdb/libtsdb.cc: Add support for counting the number of points in a time range.
    This is a very fast operation since it only has to look at the series
    index and query head and tail chunks.

--- src/libtsdb/tsdb.h
+++ src/libtsdb/tsdb.h
@@ -568,6 +568,10 @@ namespace tsdb
                        uint64_t t0, uint64_t t1, uint64_t limit);
     };
 
+    // Counts the number of points between t0 and t1, inclusive.
+    size_t count_points(const series_read_lock& read_lock, uint64_t t0,
+                        uint64_t t1);
+
     // Deletes points from the series, up to and including timestamp t.
     void delete_points(const measurement& m, const futil::path& series,
                        uint64_t t);
