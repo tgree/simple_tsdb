4029058834557162904da3b3ae52125d6af258e8
Terry Greeniaus <terrygreeniaus@gmail.com>
2025-03-19 20:48:53 -0600

    libtsdb/mean_op.cc: Factor sum low-level code out of tsdbcli into libtsdb.
    The tsdbcli code was doing a mean() operation on the data points.  A
    more generic operation is to do a sum(), while also keeping track of
    non_null() as you go (which is what mean() does internally).  So, we now
    provide a sum_op class in libtsdb which can do the two generic
    operations and then tsdbcli does the final division to convert it into
    mean().
    
    In principle, this gives us the power to do sum() and "count non_null"
    along with mean(), both in tsdbcli and any other client such as
    tsdbserver which will need mean() soon.

---
+++ src/libtsdb/sum_op.h
@@ -0,0 +1,41 @@
+// Copyright (c) 2025 by Terry Greeniaus.
+// All rights reserved.
+#ifndef __SRC_LIBTSDB_SUM_OP_H
+#define __SRC_LIBTSDB_SUM_OP_H
+
+#include "select_op.h"
+
+namespace tsdb
+{
+    // Iterator to compute the sum and the count of non-null points in each
+    // window range.  This allows computing the sum operation, the non-null
+    // count operation, and the mean operation (by dividing in the client if
+    // desired).
+    struct sum_op
+    {
+        // Query range.
+        const uint64_t  t0;
+        const uint64_t  t1;
+        const uint64_t  window_ns;
+        const uint64_t  nranges;
+
+        // Select op that we are using to iterate.
+        select_op_first op;
+        size_t          op_index;
+
+        // Latest result.
+        uint64_t                range_t0;
+        uint64_t                range_t1;
+        std::vector<double>     sums;
+        std::vector<uint64_t>   npoints;
+
+        bool next();
+
+        sum_op(const series_read_lock& read_lock,
+               const futil::path& series_id,
+               const std::vector<std::string>& field_names,
+               uint64_t t0, uint64_t t1, uint64_t window_ns);
+    };
+}
+
+#endif /* __SRC_LIBTSDB_SUM_OP_H */
