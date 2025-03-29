edda373ab216a0760c87b2a39b76b2978a56a013
Terry Greeniaus <terrygreeniaus@gmail.com>
2025-03-28 19:42:56 -0600

    libtsdb/libtsdb.cc: Use xact classes to clean up initialization.

--- src/libtsdb/libtsdb.cc
+++ src/libtsdb/libtsdb.cc
@@ -1,11 +1,13 @@
 // Copyright (c) 2025 by Terry Greeniaus.
 // All rights reserved.
 #include "tsdb.h"
+#include <futil/xact.h>
 
 void
 tsdb::init() try
 {
-    futil::mkdir("databases",0770);
+    futil::xact_mkdir databases_dir("databases",0770);
+    databases_dir.commit();
 }
 catch (const futil::errno_exception& e)
 {
