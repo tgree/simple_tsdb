9849ffe14b9f24363262132b2711417b0849d15e
Terry Greeniaus <terrygreeniaus@gmail.com>
2025-04-01 22:00:22 -0600

    scripts/third_party.mk: Add rules to generate libssl and libcrypto symlinks.

--- scripts/third_party.mk
+++ scripts/third_party.mk
@@ -4,3 +4,27 @@ $(LIB_DIR)/libz-ng.a: $(THIRD_PARTY)/zlib-ng/libz-ng.a
 	@echo Symlinking $@...
 	@mkdir -p $(LIB_DIR)
 	@ln -s ../../$(THIRD_PARTY)/zlib-ng/libz-ng.a $@
+
+ifeq ($(UNAME_S), Darwin)
+$(LIB_DIR)/libssl.a: /opt/homebrew/opt/openssl/lib/libssl.a
+	@echo Symlinking $@...
+	@mkdir -p $(LIB_DIR)
+	@ln -s $< $@
+
+$(LIB_DIR)/libcrypto.a: /opt/homebrew/opt/openssl/lib/libcrypto.a
+	@echo Symlinking $@...
+	@mkdir -p $(LIB_DIR)
+	@ln -s $< $@
+endif
+
+ifeq ($(UNAME_S), Linux)
+$(LIB_DIR)/libssl.a: /usr/lib/x86_64-linux-gnu/libssl.a
+	@echo Symlinking $@...
+	@mkdir -p $(LIB_DIR)
+	@ln -s $< $@
+
+$(LIB_DIR)/libcrypto.a: /usr/lib/x86_64-linux-gnu/libcrypto.a
+	@echo Symlinking $@...
+	@mkdir -p $(LIB_DIR)
+	@ln -s $< $@
+endif
