8b8cb0f357dc6fb13c8fb9502d914a441895fe3b
Terry Greeniaus <terrygreeniaus@gmail.com>
2025-04-07 04:20:39 -0600

    libtsdb/count.cc: Include WAL points in the count.

--- src/libtsdb/count.h
+++ src/libtsdb/count.h
@@ -14,6 +14,12 @@ namespace tsdb
         uint64_t    time_last;
     };
 
+    // Counts points from either the committed data set or the WAL.
+    count_result count_committed_points(const series_read_lock& read_lock,
+                                        uint64_t t0, uint64_t t1);
+    count_result count_wal_points(const series_read_lock& read_lock,
+                                  uint64_t t0, uint64_t t1);
+
     // Counts the number of points between t0 and t1, inclusive.
     count_result count_points(const series_read_lock& read_lock, uint64_t t0,
                               uint64_t t1);
