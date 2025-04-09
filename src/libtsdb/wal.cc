4f02af2111703b6e6c3750e5817c6aa71f375f11
Terry Greeniaus <terrygreeniaus@gmail.com>
2025-04-09 02:45:32 -0600

    libtsdb/wal.cc: Move overwrite validation into the WAL.
    The main data store now assumes that no overwrites ever take place.

--- src/libtsdb/wal.cc
+++ src/libtsdb/wal.cc
@@ -2,6 +2,7 @@
 // All rights reserved.
 #include "wal.h"
 #include "write.h"
+#include "tsdb.h"
 #include <hdr/auto_buf.h>
 #include <hdr/kmath.h>
 
@@ -15,8 +16,95 @@ tsdb::write_wal(series_write_lock& write_lock, size_t npoints,
     // Index whatever it is we are trying to write.
     write_chunk_index wci(write_lock.m,npoints,bitmap_offset,data_len,data);
 
-    // TODO: Overwrite handling.
-    kassert(wci.timestamps[0] > write_lock.time_last);
+    // Ensure that timestamps are in strictly-increasing order.
+    for (size_t i=1; i<wci.npoints; ++i)
+    {
+        if (wci.timestamps[i] <= wci.timestamps[i-1])
+            throw tsdb::out_of_order_timestamps_exception();
+    }
+
+    // Discard any previously-deleted points.
+    while (wci.timestamps[0] < write_lock.time_first)
+    {
+        if (!--wci.npoints)
+        {
+            printf("100%% previously deleted, abandoning write op.\n");
+            return;
+        }
+
+        ++wci.bitmap_offset;
+        for (size_t i=0; i<write_lock.m.fields.size(); ++i)
+        {
+            const auto* fti = &ftinfos[write_lock.m.fields[i].type];
+            wci.fields[i].data_ptr += fti->nbytes;
+        }
+    }
+
+    // Check any overwrites that exist in the main data store.  Note: this is
+    // not inconsistent with the previous loop avoce.  It is allowed to have
+    // time_first > time_last if we have deleted points that only ever existed
+    // in the WAL.
+    if (wci.timestamps[0] <= write_lock.time_last)
+    {
+        // Select all data points from the overlap.
+        uint64_t overlap_time_last = MIN(wci.timestamps[wci.npoints-1],
+                                         write_lock.time_last);
+        select_op_first op(write_lock,"<overwrite>",{},wci.timestamps[0],
+                           overlap_time_last,-1);
+        kassert(op.npoints);
+
+        // Compare all the points.
+        while (op.npoints)
+        {
+            // Start by doing a memcmp of all the timestamps.
+            if (memcmp(wci.timestamps,op.timestamps_begin,op.npoints*8))
+            {
+                printf("Overwrite mismatch in timestamps.\n");
+                throw tsdb::timestamp_overwrite_mismatch_exception();
+            }
+            wci.timestamps += op.npoints;
+
+            // Do a memcmp on all of the fields.
+            for (size_t i=0; i<write_lock.m.fields.size(); ++i)
+            {
+                const auto* fti = &ftinfos[write_lock.m.fields[i].type];
+                size_t len = op.npoints*fti->nbytes;
+                if (memcmp(wci.fields[i].data_ptr,op.field_data[i],len))
+                {
+                    printf("Overwrite mismatch in field %s.\n",
+                           write_lock.m.fields[i].name);
+                    throw tsdb::field_overwrite_mismatch_exception();
+                }
+                wci.fields[i].data_ptr += len;
+            }
+
+            // Manually check all the bitmaps.
+            for (size_t i=0; i<write_lock.m.fields.size(); ++i)
+            {
+                for (size_t j=0; j<op.npoints; ++j)
+                {
+                    if (wci.get_bitmap_bit(i,j) != op.get_bitmap_bit(i,j))
+                    {
+                        printf("Overwrite mismatch in bitmap %s.\n",
+                               write_lock.m.fields[i].name);
+                        throw tsdb::bitmap_overwrite_mismatch_exception();
+                    }
+                }
+            }
+            wci.bitmap_offset += op.npoints;
+
+            // Count how many points we drop.
+            wci.npoints -= op.npoints;
+
+            op.next();
+        }
+
+        if (!wci.npoints)
+        {
+            printf("100%% main store overwrite, abandoning write op.\n");
+            return;
+        }
+    }
 
     // Map the WAL file.
     futil::file wal_fd(write_lock.series_dir,"wal",O_RDWR);
@@ -24,18 +112,100 @@ tsdb::write_wal(series_write_lock& write_lock, size_t npoints,
     size_t entry_size = sizeof(wal_entry) + write_lock.m.fields.size()*8;
     size_t wal_nentries = wal_size/entry_size;
 
-    // TODO: Overwrite handling.
+    // Check for any overwrites in the WAL.
     if (wal_nentries)
     {
         wal_fd.lseek((wal_nentries - 1)*entry_size,SEEK_SET);
         uint64_t wal_time_last = wal_fd.read_u64();
-        kassert(wci.timestamps[0] > wal_time_last);
+
+        if (wci.timestamps[0] <= wal_time_last)
+        {
+            wal_query wq(write_lock,wci.timestamps[0],
+                         wci.timestamps[wci.npoints-1]);
+            if (wq.nentries > wci.npoints)
+            {
+                printf("WAL overwrite point count mismatch.\n");
+                throw tsdb::timestamp_overwrite_mismatch_exception();
+            }
+
+            // Check all the timestamps.
+            for (size_t i=0; i<wq.nentries; ++i)
+            {
+                if (wci.timestamps[i] != wq[i].time_ns)
+                {
+                    printf("WAL overwrite mismatch in timestamps.\n");
+                    throw tsdb::timestamp_overwrite_mismatch_exception();
+                }
+            }
+
+            // Check each bitmap.
+            for (size_t i=0; i<write_lock.m.fields.size(); ++i)
+            {
+                for (size_t j=0; j<wq.nentries; ++j)
+                {
+                    if (wci.get_bitmap_bit(i,j) != wq[j].get_bitmap_bit(i))
+                    {
+                        printf("WAL overwrite mismatch in bitmaps.\n");
+                        throw tsdb::timestamp_overwrite_mismatch_exception();
+                    }
+                }
+            }
+
+            // Check each field.
+            for (size_t i=0; i<write_lock.m.fields.size(); ++i)
+            {
+                auto& fti = ftinfos[write_lock.m.fields[i].type];
+                auto* field_data = (const void*)wci.fields[i].data_ptr;
+                for (size_t j=0; j<wq.nentries; ++j)
+                {
+                    bool match = false;
+                    switch (fti.nbytes)
+                    {
+                        case 1:
+                            match = (((uint8_t*)field_data)[j] ==
+                                    wq[j].get_field<uint8_t>(i));
+                        break;
+
+                        case 4:
+                            match = (((uint32_t*)field_data)[j] ==
+                                    wq[j].get_field<uint32_t>(i));
+                        break;
+
+                        case 8:
+                            match = (((uint64_t*)field_data)[j] ==
+                                    wq[j].get_field<uint64_t>(i));
+                        break;
+                    }
+                    if (!match)
+                    {
+                        printf("WAL overwrite mismatch in fields.\n");
+                        throw tsdb::timestamp_overwrite_mismatch_exception();
+                    }
+                }
+            }
+
+            wci.timestamps += wq.nentries;
+            for (size_t i=0; i<write_lock.m.fields.size(); ++i)
+            {
+                const auto* fti = &ftinfos[write_lock.m.fields[i].type];
+                size_t len = wq.nentries*fti->nbytes;
+                wci.fields[i].data_ptr += len;
+            }
+            wci.bitmap_offset += wq.nentries;
+            wci.npoints -= wq.nentries;
+        }
+
+        if (!wci.npoints)
+        {
+            printf("100%% WAL overwrite, abandoning write op.\n");
+            return;
+        }
     }
 
     // Convert the column-based write format into WAL row-based format.
-    auto_buf rows_buf(entry_size*npoints);
+    auto_buf rows_buf(entry_size*wci.npoints);
     wal_entry_iterator rows_iter((wal_entry*)rows_buf.data,entry_size);
-    for (size_t i=0; i<npoints; ++i)
+    for (size_t i=0; i<wci.npoints; ++i)
     {
         auto& we = *rows_iter++;
         we.time_ns = wci.timestamps[i];
@@ -78,6 +248,6 @@ tsdb::write_wal(series_write_lock& write_lock, size_t npoints,
     // 12.5K is around the middle of that range and maybe we also need to
     // update metadata from time to time.
     wal_fd.lseek(wal_nentries*entry_size,SEEK_SET);
-    wal_fd.write_all(rows_buf.data,entry_size*npoints);
+    wal_fd.write_all(rows_buf.data,entry_size*wci.npoints);
     wal_fd.fsync_and_flush();
 }
