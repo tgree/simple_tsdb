b0edada18cadb8f9b8431316ee04c74754ab75fc
Terry Greeniaus <terrygreeniaus@gmail.com>
2025-04-09 02:06:20 -0600

    src/tsdbcli: Deprecate original tsdbcli binary.

--- src/tsdbcli/module.mk
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2025 by Terry Greeniaus.
-# All rights reserved.
-SUBMODULES :=
-
-tsdbcli.MK  := $(MODULE_MK)
-tsdbcli.LIB := libtsdb.a libfutil.a libz-ng.a libssl.a libcrypto.a
-tsdbcli.OBJ := \
-	$(MODULE_BUILD_DIR)/main.o \
-	$(BUILD_O_DIR)/floor/kassert.o
