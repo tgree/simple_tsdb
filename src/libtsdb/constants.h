f4ea544bf870b8fe1d0aef0533b17c0d8743f8a3
Terry Greeniaus <terrygreeniaus@gmail.com>
2025-04-10 17:50:09 -0600

    libtsdb/root: Provide a write-throttle value in the configuration.
    This will be used to apply feedback in tsdbserver to clients that are
    performing write operations, forcing them to batch on their end so that
    SSD wear is reduced.

--- src/libtsdb/constants.h
+++ src/libtsdb/constants.h
@@ -12,6 +12,11 @@ KASSERT(is_pow2(DEFAULT_CHUNK_SIZE_M));
 // Maximum number of entries in the WAL.
 #define DEFAULT_WAL_MAX_ENTRIES     128
 
+// How long for tsdbserver to delay between write requests on the same
+// connection.  This reduces wear on the SSD by delaying acknowledgements back
+// to the source, forcing the source to buffer data.
+#define DEFAULT_WRITE_THROTTLE_NS   0
+
 // Maximum number of fields in a measurement.  This is partly defined by things
 // like the size of the index field in a schema_entry, so increasing this past
 // 64 here is insufficient; reducing it below 64 should be fine.
