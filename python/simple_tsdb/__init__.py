e58f4f4b7b2798ab9fb5847c60d90f9c28c410c4
Terry Greeniaus <terrygreeniaus@gmail.com>
2025-04-11 21:49:17 -0600

    simple_tsdb/client.py: Rename tsdb_client.py -> client.py.

--- python/simple_tsdb/__init__.py
+++ python/simple_tsdb/__init__.py
@@ -1,10 +1,10 @@
 # Copyright (c) 2025 by Terry Greeniaus.
 # All rights reserved.
-from .tsdb_client import (Client,
-                          StatusException,
-                          StatusCode,
-                          ConnectionClosedException,
-                          ProtocolException)
+from .client import (Client,
+                     StatusException,
+                     StatusCode,
+                     ConnectionClosedException,
+                     ProtocolException)
 
 
 __all__ = [
