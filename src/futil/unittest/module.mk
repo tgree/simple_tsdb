eb33542fcf080ee7c72bdab7dea800f40e3aee48
Terry Greeniaus <terrygreeniaus@gmail.com>
2025-04-26 21:31:17 -0600

    unittest/tcp_test.cc: Add simple client-server TCP test.
    Yes, one can apparently do client/server comms in a single thread.

--- src/futil/unittest/module.mk
+++ src/futil/unittest/module.mk
@@ -2,9 +2,13 @@
 # All rights reserved.
 TESTS := \
 	 futil_test \
-	 sockaddr_test
+	 sockaddr_test \
+	 tcp_test
 
 futil_test.OBJ := \
 	$(MODULE_TBUILD_DIR)/futil_test.o
 sockaddr_test.OBJ := \
 	$(MODULE_TBUILD_DIR)/sockaddr_test.o
+tcp_test.OBJ := \
+	$(MODULE_TBUILD_DIR)/tcp_test.o \
+	$(BUILD_O_DIR)/floor/kassert.o
