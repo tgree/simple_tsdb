a42294c8db33c43da2d4e29baccf8db14c8bb619
Terry Greeniaus <terrygreeniaus@gmail.com>
2025-08-08 22:36:17 -0600

    grafana/tgree-simpletsdb-datasource: Add support for (and require) SSL and authentication.
    This change adds hostname/username/password fields to the data source
    configuration editor and then uses them to connect via SSL (instead of
    plain TCP) and then authenticate with the database.  This is required so
    that we can connect to the same simple_tsdb instance that is receiving
    data from remote data sources such as sensors.
    
    If Grafana and the simple_tsdb instance are running on the same server,
    then the server should have an entry in its /etc/hosts file to loopback
    to itself for its own hostname.

--- grafana/tgree-simpletsdb-datasource/src/components/ConfigEditor/ConfigEditor.tsx
+++ grafana/tgree-simpletsdb-datasource/src/components/ConfigEditor/ConfigEditor.tsx
@@ -1,21 +1,73 @@
 import React, { ReactElement } from 'react';
-import { InlineField, Input } from '@grafana/ui';
+import { InlineField, Input, SecretInput } from '@grafana/ui';
 import type { EditorProps } from './types';
 import { useChangeOptions } from './useChangeOptions';
+import { useChangeSecureOptions } from './useChangeSecureOptions';
 
 export function ConfigEditor(props: EditorProps): ReactElement {
-  const { jsonData } = props.options;
+  const { onOptionsChange, options } = props;
+  const { jsonData, secureJsonFields, secureJsonData } = options;
   const onDatabaseFieldChange = useChangeOptions(props, 'database');
+  const onHostnameFieldChange = useChangeOptions(props, 'hostname');
+  const onUsernameFieldChange = useChangeOptions(props, 'username');
+  const onPasswordFieldChange = useChangeSecureOptions(props, 'password');
+
+  const onPasswordFieldReset = () => {
+    onOptionsChange({
+      ...options,
+      secureJsonFields: {
+        ...options.secureJsonFields,
+        password: false,
+      },
+      secureJsonData: {
+        ...options.secureJsonData,
+        password: '',
+      },
+    });
+  };
 
   return (
     <>
       <InlineField label="Database" labelWidth={14} interactive tooltip={'Database'}>
         <Input
+          required
           id="config-editor-database"
           onChange={onDatabaseFieldChange}
           value={jsonData.database}
           placeholder="Enter the database"
           width={40}
+        />
+      </InlineField>
+      <InlineField label="Hostname" labelWidth={14} interactive tooltip={'Hostname'}>
+        <Input
+          required
+          id="config-editor-hostname"
+          onChange={onHostnameFieldChange}
+          value={jsonData.hostname}
+          placeholder="Enter the server hostname:port (normally :4000)"
+          width={40}
+        />
+      </InlineField>
+      <InlineField label="Username" labelWidth={14} interactive tooltip={'Username'}>
+        <Input
+          required
+          id="config-editor-username"
+          onChange={onUsernameFieldChange}
+          value={jsonData.username}
+          placeholder="Enter the user name to log in with"
+          width={40}
+        />
+      </InlineField>
+      <InlineField label="Password" labelWidth={14} interactive tooltip={'Password'}>
+        <SecretInput
+          required
+          id="config-editor-password"
+          isConfigured={secureJsonFields.password}
+          value={secureJsonData?.password}
+          placeholder="Enter the password to log in with"
+          width={40}
+          onReset={onPasswordFieldReset}
+          onChange={onPasswordFieldChange}
         />
       </InlineField>
     </>
