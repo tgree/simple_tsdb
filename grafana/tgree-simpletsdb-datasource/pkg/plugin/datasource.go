4cc0748f2bf34ee5bdb59939519fae60f1fe1230
Terry Greeniaus <terrygreeniaus@gmail.com>
2025-08-10 14:28:29 -0600

    plugin/datasource.go: Don't return all-nil data frames to Grafana.
    If we do a query which returns data points, but all the data points are
    nil (maybe we have an uncalibrated sensor so it just returns frequencies
    but not converted values and our panel is trying to plot converted
    values), don't return an all-nil data frame to Grafana.  If we return
    the all-nil frame then we end up with a series name in the plot's legend
    but no visible data for that series, which is rather annoying.

--- grafana/tgree-simpletsdb-datasource/pkg/plugin/datasource.go
+++ grafana/tgree-simpletsdb-datasource/pkg/plugin/datasource.go
@@ -271,6 +271,7 @@ func (d *Datasource) querySelect(tc *TSDBClient, database string, measurement st
 	// Pull chunks of data from the server and append them to our running data.
 	timestamps := []time.Time{}
 	ptrs := schema.MakePtrArray(field)
+	all_nil := true
 	for {
 		rxc, err := op.ReadChunk()
 		if err != nil {
@@ -284,7 +285,16 @@ func (d *Datasource) querySelect(tc *TSDBClient, database string, measurement st
 			timestamps = append(timestamps, time.Unix(0, int64(time_ns)))
 		}
 
-		ptrs = rxc.AppendToArray(ptrs)
+		var chunk_all_nil bool
+		ptrs, chunk_all_nil = rxc.AppendToArray(ptrs)
+		if !chunk_all_nil {
+			all_nil = false
+		}
+	}
+	
+	// If no data, return empty frame.
+	if all_nil {
+		return nil, nil
 	}
 
 	// Return the response.
@@ -308,6 +318,7 @@ func (d *Datasource) queryMean(tc *TSDBClient, database string, measurement stri
 	means := []float64{}
 	ptrs := []*float64{}
 	chunk_base := uint64(0)
+	total_points := uint64(0)
 	for {
 		rxc, err := op.ReadChunk()
 		if err != nil {
@@ -328,6 +339,7 @@ func (d *Datasource) queryMean(tc *TSDBClient, database string, measurement stri
 			}
 		}
 		for i := uint16(0); i < rxc.nbuckets; i++ {
+			total_points += rxc.npoints[i]
 			if rxc.npoints[i] == 0 {
 				ptrs = append(ptrs, nil)
 			} else {
@@ -336,6 +348,11 @@ func (d *Datasource) queryMean(tc *TSDBClient, database string, measurement stri
 		}
 
 		chunk_base += uint64(rxc.nbuckets)
+	}
+
+	// If no data, return empty frame.
+	if total_points == 0 {
+		return nil, nil
 	}
 
 	// Return the response.
@@ -1406,8 +1423,9 @@ func (self *RXChunk) IsNull(i uint32) bool {
 	return (self.bitmap[bitmap_index] & (1 << shift)) == 0
 }
 
-func (self *RXChunk) AppendToArray(ptrs interface{}) interface{} {
+func (self *RXChunk) AppendToArray(ptrs interface{}) (interface{}, bool) {
 	p := unsafe.Pointer(&self.data[self.data_offset])
+	all_nil := true
 	switch ptrs.(type) {
 	case []*float64:
 		vf64 := unsafe.Slice((*float64)(p), self.npoints)
@@ -1416,6 +1434,7 @@ func (self *RXChunk) AppendToArray(ptrs interface{}) interface{} {
 				ptrs = append(ptrs.([]*float64), nil)
 			} else {
 				ptrs = append(ptrs.([]*float64), &vf64[i])
+				all_nil = false
 			}
 		}
 
@@ -1426,6 +1445,7 @@ func (self *RXChunk) AppendToArray(ptrs interface{}) interface{} {
 				ptrs = append(ptrs.([]*float32), nil)
 			} else {
 				ptrs = append(ptrs.([]*float32), &vf32[i])
+				all_nil = false
 			}
 		}
 
@@ -1436,6 +1456,7 @@ func (self *RXChunk) AppendToArray(ptrs interface{}) interface{} {
 				ptrs = append(ptrs.([]*uint64), nil)
 			} else {
 				ptrs = append(ptrs.([]*uint64), &vu64[i])
+				all_nil = false
 			}
 		}
 
@@ -1446,6 +1467,7 @@ func (self *RXChunk) AppendToArray(ptrs interface{}) interface{} {
 				ptrs = append(ptrs.([]*uint32), nil)
 			} else {
 				ptrs = append(ptrs.([]*uint32), &vu32[i])
+				all_nil = false
 			}
 		}
 
@@ -1456,6 +1478,7 @@ func (self *RXChunk) AppendToArray(ptrs interface{}) interface{} {
 				ptrs = append(ptrs.([]*int64), nil)
 			} else {
 				ptrs = append(ptrs.([]*int64), &vi64[i])
+				all_nil = false
 			}
 		}
 
@@ -1466,6 +1489,7 @@ func (self *RXChunk) AppendToArray(ptrs interface{}) interface{} {
 				ptrs = append(ptrs.([]*int32), nil)
 			} else {
 				ptrs = append(ptrs.([]*int32), &vi32[i])
+				all_nil = false
 			}
 		}
 
@@ -1476,6 +1500,7 @@ func (self *RXChunk) AppendToArray(ptrs interface{}) interface{} {
 				ptrs = append(ptrs.([]*uint8), nil)
 			} else {
 				ptrs = append(ptrs.([]*uint8), &vu8[i])
+				all_nil = false
 			}
 		}
 
@@ -1484,7 +1509,7 @@ func (self *RXChunk) AppendToArray(ptrs interface{}) interface{} {
 		panic("Unknown field type!")
 	}
 
-	return ptrs
+	return ptrs, all_nil
 }
 
 func (self *RXChunk) String() string {
