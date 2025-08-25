e5c92805a9085c43262051937a4f56beadc54618
Terry Greeniaus <terrygreeniaus@gmail.com>
2025-08-25 02:13:11 -0600

    libtsdb/unittest: Add root_test.cc.

--- src/libtsdb/unittest/module.mk
+++ src/libtsdb/unittest/module.mk
@@ -1,7 +1,14 @@
 # Copyright (c) 2025 by Terry Greeniaus.
 # All rights reserved.
 TESTS := \
-	 bitmap_test
+	 bitmap_test \
+	 root_test
 
 bitmap_test.OBJ := \
 	$(MODULE_TBUILD_DIR)/bitmap_test.o
+root_test.LIB := libcrypto.a
+root_test.OBJ := \
+	$(MODULE_TBUILD_DIR)/root_test.o \
+	$(PARENT_TBUILD_DIR)/root.o \
+	$(BUILD_TO_DIR)/futil/fakefs/fakefs.o \
+	$(BUILD_O_DIR)/floor/kassert.o
