f27bfb0caa9373da558eb250e525b2b6da06e66c
Terry Greeniaus <terrygreeniaus@gmail.com>
2025-08-25 02:13:13 -0600

    libtsdb/count.cc: Don't manually read time_last.
    The count() code was loading time_last directly from disk as a holdover
    from before it was recorded in the series lock.  Since we already have
    it in te lock, avoid unnecessarily re-reading it from disk.

--- src/libtsdb/count.cc
+++ src/libtsdb/count.cc
@@ -10,19 +10,17 @@ tsdb::count_committed_points(const series_read_lock& read_lock, uint64_t t0,
     uint64_t t1)
 {
     // Validate the time and limit ranges.
-    uint64_t time_last = futil::file(read_lock.series_dir,"time_last",
-                                     O_RDONLY).read_u64();
-    if (read_lock.time_first > time_last)
-        return count_result{0,read_lock.time_first,time_last};
+    if (read_lock.time_first > read_lock.time_last)
+        return count_result{0,read_lock.time_first,read_lock.time_last};
 
     t0 = MAX(t0,read_lock.time_first);
-    t1 = MIN(t1,time_last);
+    t1 = MIN(t1,read_lock.time_last);
     if (t0 > t1)
         return count_result{0,t0,t1};
     if (t1 < read_lock.time_first)
         return count_result{0,t0,read_lock.time_first};
-    if (t0 > time_last)
-        return count_result{0,time_last,t1};
+    if (t0 > read_lock.time_last)
+        return count_result{0,read_lock.time_last,t1};
 
     // Map the index.
     futil::file index_fd(read_lock.series_dir,"index",O_RDONLY);
