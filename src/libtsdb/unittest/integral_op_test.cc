5102f78f29e59b6c1a5e674a0f208f2befee5344
Terry Greeniaus <terrygreeniaus@gmail.com>
2025-08-26 21:10:51 -0600

    libtsdb/unittest: Add trivial integral_op_test.cc demonstrating bug.
    The stack trace is nonsense, but the bug is that we are accessing
    op.fields with a field_index value from the schema rather than the index
    from the query.

---
+++ src/libtsdb/unittest/integral_op_test.cc
@@ -0,0 +1,44 @@
+// Copyright (c) 2025 by Terry Greeniaus.
+// All rights reserved.
+#include "../integral_op.h"
+#include "../database.h"
+#include "../series.h"
+#include "fake_db.h"
+#include <futil/fakefs/fakefs.h>
+#include <hdr/types.h>
+#include <tmock/tmock.h>
+#include <limits>
+
+static void
+generate_db()
+{
+    init_db(16,128);
+
+    // Populate the database.  We do a 45-point write followed by an 8-point
+    // write, yielding 3 chunk entries and some WAL points:
+    //
+    //  CH0 100 - 250 [16]
+    //  CH1 260 - 410 [16]
+    //  CH2 420 - 540 [13]
+    //  WAL 550 - 620 [8]
+    //
+    // Then we write 8 points, which will all end
+    // up in the WAL.
+    populate_db(100,10,{45,8});
+}
+
+class tmock_test
+{
+    TMOCK_TEST_EXPECT_FAILURE_SHOULD_PASS(test_integral_full)
+    {
+        generate_db();
+
+        tsdb::root root(".",false);
+        tsdb::database db1(root,"db1");
+        tsdb::measurement m1(db1,"measurement1");
+        tsdb::series_read_lock srl(m1,"series1");
+        tsdb::integral_op op(srl,"series1",{"field3"},0,-1);
+    }
+};
+
+TMOCK_MAIN();
