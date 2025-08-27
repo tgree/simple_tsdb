adac282b143a629556daf7ab24ec5a8911610329
Terry Greeniaus <terrygreeniaus@gmail.com>
2025-08-26 22:05:50 -0600

    libtsdb/tsdb.h: Bump to version 1.0.5.

--- src/libtsdb/tsdb.h
+++ src/libtsdb/tsdb.h
@@ -3,8 +3,8 @@
 #ifndef __SRC_LIBTSDB_TSDB_H
 #define __SRC_LIBTSDB_TSDB_H
 
-#define SIMPLE_TSDB_VERSION         0x104
-#define SIMPLE_TSDB_VERSION_STR     "1.0.4"
+#define SIMPLE_TSDB_VERSION         0x105
+#define SIMPLE_TSDB_VERSION_STR     "1.0.5"
 
 #include "exception.h"
 #include "root.h"
